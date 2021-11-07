9457fc31d10e519d0fc21f5b1a6a0c6455635e02
from_unknown <from.midnight.walker@gmail.com>
2021-11-07 20:07:56 +0900

    カード枚数が多い時にAPIコール数を割り出して何回チェックすればよいか計算してObserveし続ける

--- js/script.js
+++ js/script.js
@@ -15,11 +15,17 @@ const paintCards = column => {
 }
 
 const checkLoadingCards = () => {
-	const columnCards = document.querySelectorAll(".js-project-column-cards")
+	// カード枚数を取得するためにproject-column毎に取得する
+	const columnCards = document.querySelectorAll(".project-column")
 
 	const config = {childList: true};
 	columnCards.forEach(column => {
+		// Counterからカード枚数を取得する
+		const numOfCards = parseInt(column.querySelector(".Counter").getAttribute("title"), 10)
+		// カードのリストを取得する（既に読み込まれていないかチェック）
 		const cards = column.querySelectorAll(".project-card")
+		// Observe対象の最小のカラムを取得する
+		const cardsColumn = column.querySelector(".js-project-column-cards")
 		if (cards.length > 0) {
 			// ネットをスロットリングすると拡張機能が画面のロード後に実行されるので既に存在する場合は色を付けて終了
 			// console.log("already cards exists")
@@ -27,20 +33,23 @@ const checkLoadingCards = () => {
 		} else {
 			// カードがない場合はまだ読み込まれていないのでObserverを設定する
 			// console.log("need to observe")
+			const NUM_OF_CARDS_LOADED_EACH_TIME = 25.0
+			// 一度に読み込まれるカード枚数から何回APIコールされるか割り出す
+			const numOfAPICall = Math.ceil(numOfCards / NUM_OF_CARDS_LOADED_EACH_TIME);
+			let counter = 0
 			const observer = new MutationObserver((mutationsList, observer) => {
 				// 初回カードが追加される時はローディングが取り除かれてカードが追加されるため以下のチェックを入れる
 				// カードがなくドラッグ＆ドロップされた時はremovedNodesが存在しないのでカードの色を変えない
+				paintCards(cardsColumn)
+				counter++
 				if (
-					mutationsList[0].removedNodes.length > 0 &&
-					mutationsList[0].addedNodes.length > 0
+					counter === numOfAPICall
 				) {
-					paintCards(column)
+					// 必要回数実行したらObserverを停止する
+					observer.disconnect()
 				}
-
-				// 一回実行したらObserverを停止する
-				observer.disconnect()
 			});
-			observer.observe(column, config)
+			observer.observe(cardsColumn, config)
 		}
 	})
 };
